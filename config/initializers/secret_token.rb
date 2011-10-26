c615c18012f4cab9d9ecf02c94395a85d29e6b13
Chris Wade <cwademusic@gmail.com>
2011-10-26 14:52:49 -0500

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummygen::Application.config.secret_token = 'e788bfdc6438595811881c429c7d0c6036696860af8f630a04d7b0a1b5e9b20f33aaa1aac5a2d10305a42a32a28f501d465672790dcb6edf9d8cd4b0f8fa451f'
