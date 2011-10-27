30c2f4caa9b0c599259eaf53d06c8b15c9a60305
Chris Wade <cwademusic@gmail.com>
2011-10-27 17:31:54 -0500

    creating static layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,16 @@ class HomeController < ApplicationController
   def index
     @users = User.all
   end
+  
+  def create
+    @user = User.new
+  end
+  
+  def show
+    @user = User.params([:id])
+  end
+  
+  #def delete
+  #  @user = User.params([:id]).delete(:method => delete)
+  #end
 end
